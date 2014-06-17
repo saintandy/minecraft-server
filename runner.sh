2b71dffc7067c1150836345610d7f3eab0818deb
Vlad Tarniceru <vladtarniceru@gmail.com>
2014-06-17 15:06:28 +0300

    auto-backup

--- runner.sh
+++ runner.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 cd "$( dirname "$0" )"
 java -Xmx1024M -jar minecraft_server.1.7.9.jar -o true
+
+while true
+do
+	git add ~/Desktop/minecraft-server/*
+	git commit -m "auto-backup"
+    git push -u origin master
+	sleep 600
+done
